48e650b1b297b078f9dcd7503de6c97293ea4790
Arya Tajdar <tajdararya@gmail.com>
2025-12-29 17:00:25 +0330

    Section2

---
+++ section2.js
@@ -0,0 +1,120 @@
+// let arian = 2
+// arian = 10
+// arian = 'hi'
+// console.log(arian)
+
+// console.
+// const kobra = 5;
+// console.log(kobra)
+
+console.clear()
+
+let num = 2e3
+// console.log(typeof num)
+
+let str = "ksldmvl546115@#%^$" 
+// console.log(typeof str)
+
+let bool = true // false
+// console.log(typeof bool)
+
+// console.log(test)
+// var test = 5
+// console.log(test)
+
+let und = undefined
+// console.log(typeof und)
+
+let nu = null
+// console.log(typeof nu)
+
+// > < >= <= ==
+// console.log(2 < 0)
+
+let check = 13
+
+// if(check > 10){
+//     console.log(25)
+// }else if(check == 5){
+//     console.log('check is Five')
+// }else{
+//     console.log('Sina')
+// }
+
+// if(typeof result == 'number'){
+    //     console.log(true)
+    // }else{
+        //     console.log(false)
+        // }
+        
+        
+        // typeof result == 'number' ? console.log(true) : console.log(false)
+        
+        
+        // console.log(typeof result == 'number')
+        
+        // if(isNaN(result)){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// } // Not a Number
+
+let result = '4042'
+console.clear()
+
+// if(typeof result == 'string' && result == '4041'){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+// if(typeof result == 'string' && result == '4041'){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+
+// let a = 26
+// let b = 72
+// let c = 49
+
+// if(a > b && a > c){
+//     console.log(a)
+// }else if(b > a && b > c){
+//     console.log(b)
+// }else{
+//     console.log(c)
+// }
+
+let email = "maiSA@gmail.com"
+
+// console.log(email.length)
+// console.log(email.toLowerCase())
+// console.log(email.toUpperCase())
+// console.log(email.startsWith('m'))
+// console.log(email.endsWith('@gmail.com'))
+// console.log(email.includes('@gmail'))
+// console.log(email.trim())
+// console.log(email.replace('@gmail' , '@yahoo'))
+// console.log(email.indexOf('.'))
+// console.log(email.slice(0,11))
+
+let num2 = 225.445454
+// console.log(num.toExponential())
+// console.log(num2.toFixed())
+// console.log(typeof num2.toString())
+
+let StringNum2 = num2.toString()
+console.log(typeof StringNum2)
+
+let data = '52.6'
+if(typeof data == ' string'){
+    console.log(1)
+}else{
+    console.log(0)
+}
+
+
+
+
+
